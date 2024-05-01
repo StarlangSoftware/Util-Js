831e1f1caebdb0d0937a0cb91a36d335528a2806
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-05-01 12:38:05 +0300

    Updated the documentation.

--- dist/RandomArray.d.ts
+++ dist/RandomArray.d.ts
@@ -7,5 +7,12 @@ export declare class RandomArray {
      * @param itemCount integer input defining array size.
      */
     static normalizedArray(itemCount: number): Array<number>;
+    /**
+     * Creates and returns a random index array, where the indexes in the array are between 0 and itemCount - 1. For
+     * example, if itemCount is 4, the method returns an array of indexes 0, 1, 2, 3 but shuffled.
+     * @param itemCount Number of indexes
+     * @param seed Random seed
+     * @return Shuffled array containing numbers between 0 and itemCount - 1.
+     */
     static indexArray(itemCount: number, seed: number): Array<number>;
 }
--- dist/RandomArray.js
+++ dist/RandomArray.js
@@ -33,6 +33,13 @@
             }
             return array;
         };
+        /**
+         * Creates and returns a random index array, where the indexes in the array are between 0 and itemCount - 1. For
+         * example, if itemCount is 4, the method returns an array of indexes 0, 1, 2, 3 but shuffled.
+         * @param itemCount Number of indexes
+         * @param seed Random seed
+         * @return Shuffled array containing numbers between 0 and itemCount - 1.
+         */
         RandomArray.indexArray = function (itemCount, seed) {
             var array = new Array(itemCount);
             for (var i = 0; i < itemCount; i++) {
